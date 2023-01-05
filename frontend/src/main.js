6d9857f3f30550756b5dfc1de0b7e008aacbd116
raspudic97 <raspudic88@gmail.com>
2023-01-05 19:21:57 +0100

    Frontend setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import App from "./App.vue";
+import "./assets/tailwind.css";
 
-createApp(App).mount('#app')
+import * as Vue from "vue"; // in Vue 3
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+const app = Vue.createApp(App).mount("#app");
+app.use(VueAxios, axios);
